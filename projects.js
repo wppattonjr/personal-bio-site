fb7f86206d95735f2fdd581bb33daa6979640b5a
Wendell <wendell.pattonjr@gmail.com>
2020-07-25 18:33:30 -0500

    played around with css to see if I could get the cards to display properly

--- projects.js
+++ projects.js
@@ -11,7 +11,6 @@ const projects = [
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
 
     },
-    
     {
         title: "Cool Project 2", 
         screenshot: "https://greenlightcorp.com/wp-content/uploads/2019/07/Shall-we-play-a-game.jpg", 
@@ -22,7 +21,6 @@ const projects = [
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
 
     },
-    
     {
         title: "Cool Project 3", 
         screenshot: "https://greenlightcorp.com/wp-content/uploads/2019/07/Shall-we-play-a-game.jpg", 
@@ -31,8 +29,12 @@ const projects = [
         available: true,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-
     }
+    
+    
+    
+
+    
 ]
 
 const printToDom = (divId, textToPrint) => {
