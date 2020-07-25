21a81239eed2acf31daee668d00c9cc38197fcea
Wendell <wendell.pattonjr@gmail.com>
2020-07-24 20:32:37 -0500

    added functions for createing project cards and printing to the dom. also did some styling on the project cards

--- projects.js
+++ projects.js
@@ -8,7 +8,7 @@ let projects = [
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
         available: true,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
 
     },
     {
@@ -42,6 +42,8 @@ let projects = [
 ];
 
 const printToDom = (elementId, textToPrint) => {
+    const selectedDiv = document.getElementById(elementId);
+    selectedDiv.innerHTML = textToPrint;
 
 }
 
@@ -49,6 +51,19 @@ const createProjectCards = () => {
     let domString ="";
 
     for (let i=0; i < projects.length; i++) {
+        domString += `<div class="projects">`;
+        domString +=    `<div class="project-title">Project Name: ${projects[i].title}</div>`
+        domString +=    `<div class="project-screenshot"><img src=${projects[i].screenshot}</div>`
+        domString +=    `<div class="project-description">${projects[i].description}</div>`
+        domString +=    `<div class="project-technologiesUsed">Technologies Used: ${projects[i].technologiesUsed}</div>`
+        domString +=    `<div class="project-availability">Is Project Available: ${projects[i].available}</div>`
+        domString +=    `<div class="project-url">${projects[i].url}</div>`
+        domString +=    `<div class="project-githubUrl">GitHub Url: ${projects[i].githubUrl}</div>`
+        domString += `</div>`
 
     }
+
+    printToDom("projects", domString)
 }
+
+createProjectCards()
